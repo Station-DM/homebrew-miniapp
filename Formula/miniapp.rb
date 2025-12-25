cdb6c103c80741b44970588de2f619397879ab97
zhou_jun <zhou_jun@internal.dev>
2025-12-25 10:58:19 +0800

    miniapp v0.1.0

---
+++ Formula/miniapp.rb
@@ -0,0 +1,18 @@
+class Miniapp < Formula
+  desc "MiniApp Host SDK integration helper CLI"
+  homepage "https://github.com/Station-DM/miniapp-cli"
+  url "https://github.com/Station-DM/miniapp-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "b35386d8cd309e45fa63849823720607634b7b84703b2afa018620fa83b35e83"
+
+  depends_on macos: :ventura
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/miniapp"
+  end
+
+  test do
+    output = shell_output("#{bin}/miniapp --help")
+    assert_match "miniapp host sdk install", output
+  end
+end
