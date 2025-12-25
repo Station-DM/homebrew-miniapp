fc639ab63d9625732fb1c19ecb684e093468658e
zhou_jun <zhou_jun@internal.dev>
2025-12-25 11:48:27 +0800

    miniapp 0.1.1

--- Formula/miniapp.rb
+++ Formula/miniapp.rb
@@ -1,8 +1,8 @@
 class Miniapp < Formula
   desc "Host SDK integration helper for mini apps"
   homepage "https://github.com/Station-DM/miniapp-cli"
-  url "https://github.com/Station-DM/miniapp-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b35386d8cd309e45fa63849823720607634b7b84703b2afa018620fa83b35e83"
+  url "https://github.com/Station-DM/miniapp-cli/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "a16f273d0e28eebeae4142e81d61c55cd4daec54c71553dfe7a17c85dec1db5d"
   license "MIT"
 
   depends_on macos: :ventura
